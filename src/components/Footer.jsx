1833badf0cadedad538b838117e4fb668ce5f4e0
GarmontDev <164194582+GarmontDev@users.noreply.github.com>
2024-04-18 19:58:16 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return(
     <>
       <div className="relative">
-        <footer className="bg-white rounded-lg shadow m-4 absolute top-0 left-1/2">
+        <footer className="bg-white rounded-lg shadow m-4">
           <div className="w-full p-4 md:flex mx-auto max-w-screen-xl md:items-center md:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2024 
