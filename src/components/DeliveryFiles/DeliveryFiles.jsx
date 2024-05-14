dbb133c7e3f69bfb1f5590be9261a7f7bcadda4b
Carlos GAMO <garmontdev@gmail.com>
2024-05-14 09:22:50 +0200

    Adds SearchIcon

--- src/components/DeliveryFiles/DeliveryFiles.jsx
+++ src/components/DeliveryFiles/DeliveryFiles.jsx
@@ -1,4 +1,5 @@
 import "./DeliveryFiles.css"
+import SearchIcon from "../../assets/icons/SearchIcon.jsx"
 import { useState } from "react";
 import FilesList from "./FilesList.jsx";
 import FilesMenu from "./FIlesMenu.jsx";
@@ -12,9 +13,8 @@ const DeliveryFiles = ({employee, setEmployee}) => {
       <FilesMenu employee={employee} setEmployee={setEmployee} showVisibleFiles={showVisibleFiles} setShowVisibleFiles={setShowVisibleFiles}/>
       {!showVisibleFiles ? 
         <div className="flex">
-          <span className="p-1 pr-2">
-            Search Icon
-            {/* TODO */}
+          <span className="p-1 pr-2 mt-3 ml-1">
+            <SearchIcon/>
           </span>
           <input 
             className="text-gray-800 rounded-lg border-2 shadow pt-1 pb-1 pl-4 h-10 mt-2 w-full focus:outline-blue-700"
