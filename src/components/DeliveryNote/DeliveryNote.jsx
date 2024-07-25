0dee0f121efc268ea3721f3cc2b40ff6afc1cef1
GarmontDev <164194582+GarmontDev@users.noreply.github.com>
2024-07-25 10:23:50 +0200

    Fixes DeliveryNote header margins

--- src/components/DeliveryNote/DeliveryNote.jsx
+++ src/components/DeliveryNote/DeliveryNote.jsx
@@ -175,7 +175,7 @@ const DeliveryNote = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
             onChange={(e) => handleSelectChange(e.target.value)}
           >
-            <option value="all-filter" selected>
+            <option defaultValue="all-filter" selected>
               Todas
             </option>
             <option value="pending-filter">Pendientes</option>
